3f6729d81fb025aca582d060a9e713743dd413fe
anantram Kumar <anantramkumar5@gmail.com>
2020-06-07 10:55:54 -0400

    Initial Commit

---
+++ Zeenat/src/main/java/org/anantram/zeenat/ZeenatApplication.java
@@ -0,0 +1,13 @@
+package org.anantram.zeenat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeenatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeenatApplication.class, args);
+	}
+
+}
---
+++ Zeenat/src/test/java/org/anantram/zeenat/ZeenatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anantram.zeenat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeenatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
